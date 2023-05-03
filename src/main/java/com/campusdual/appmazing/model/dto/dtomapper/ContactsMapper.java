81977cd34b36858a048119bffd387daece1d1606
GuillenVidal <guillenvidal2002@gmail.com>
2023-05-03 13:33:42 +0200

    Model y Hibernate OK

---
+++ src/main/java/com/campusdual/appmazing/model/dto/dtomapper/ContactsMapper.java
@@ -0,0 +1,19 @@
+package com.campusdual.appmazing.model.dto.dtomapper;
+
+import com.campusdual.appmazing.model.Contacts;
+import com.campusdual.appmazing.model.dto.ContactsDTO;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+import java.util.List;
+
+@Mapper
+public interface ContactsMapper {
+    ContactsMapper INSTANCE = Mappers.getMapper(ContactsMapper.class);
+
+    ContactsDTO toDTO(Contacts contacts);
+
+    List<ContactsDTO> toDTOList(List<ContactsDTO> contactosList);
+
+    Contacts toEntity(ContactsDTO contactsDTO);
+}
