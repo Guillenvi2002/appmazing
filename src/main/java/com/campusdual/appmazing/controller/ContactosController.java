345562d97f9dfd227afbe81d9cf1103f06a6ea4f
GuillenVidal <guillenvidal2002@gmail.com>
2023-05-04 12:29:12 +0200

    Controladores y servicio OK

--- src/main/java/com/campusdual/appmazing/controller/ContactosController.java
+++ src/main/java/com/campusdual/appmazing/controller/ContactosController.java
@@ -1,10 +1,19 @@
 package com.campusdual.appmazing.controller;
 
+import com.campusdual.appmazing.api.IContactsService;
+import com.campusdual.appmazing.model.dto.ContactsDTO;
+import com.campusdual.appmazing.model.dto.ProductDTO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController()
 @RequestMapping("/contactos")
 public class ContactosController {
+
+    @Autowired
+    private IContactsService contactsService;
     @GetMapping
     public String testController(){
         return "Contacts controller works!";
@@ -14,4 +23,38 @@ public class ContactosController {
     public String testController(@RequestBody String name){
         return "Contacts controller works, " +name + "!";
     }
+
+    @PostMapping(value = "/get")
+    public ContactsDTO queryContacts(@RequestBody ContactsDTO contactsDTO){
+        return contactsService.queryContact(contactsDTO);
+    }
+
+    @GetMapping(value = "/getAll")
+    public List<ContactsDTO> queryAllContacts(){
+        return contactsService.queryAllContacts();
+    }
+
+    @PostMapping(value = "/add")
+    public int addContact(@RequestBody ContactsDTO contactsDTO){
+        return contactsService.insertContact(contactsDTO);
+    }
+
+    @PostMapping(value = "/addAndShow")
+    public ContactsDTO addContactAndShow(@RequestBody ContactsDTO contactsDTO){
+        int idContact = contactsService.insertContact(contactsDTO);
+        ContactsDTO newContact = new ContactsDTO();
+        newContact.setId(idContact);
+        return contactsService.queryContact(newContact);
+    }
+
+    @PutMapping(value = "/update")
+    public int updateContact(@RequestBody ContactsDTO contactsDTO){
+        return contactsService.updateContact(contactsDTO);
+    }
+
+    @DeleteMapping(value = "/delete")
+    public int deleteContact(@RequestBody ContactsDTO contactsDTO){
+        return contactsService.deleteContact(contactsDTO);
+    }
+
 }
--- src/main/java/com/campusdual/appmazing/controller/ProductController.java
+++ src/main/java/com/campusdual/appmazing/controller/ProductController.java
@@ -1,11 +1,19 @@
 package com.campusdual.appmazing.controller;
 
+import com.campusdual.appmazing.api.IProductService;
+import com.campusdual.appmazing.model.Product;
+import com.campusdual.appmazing.model.dto.ProductDTO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController()
 @RequestMapping("/products")
 public class ProductController {
 
+    @Autowired
+    private IProductService productService;
     @GetMapping
     public String testController(){
         return "Product controller works!";
@@ -15,4 +23,43 @@ public class ProductController {
     public String testController(@RequestBody String name){
         return "Product controller works, " +name + "!";
     }
+
+    @GetMapping(value = "/testMethod")
+    public String testControllerMethod(){
+        return "Products controller method works";
+    }
+
+    @PostMapping(value = "/get")
+    public ProductDTO queryProduct(@RequestBody ProductDTO productDTO){
+        return productService.queryProduct(productDTO);
+    }
+
+    @GetMapping(value = "/getAll")
+    public List<ProductDTO> queryAllProducts(){
+        return productService.queryAllProducts();
+    }
+
+    @PostMapping(value = "/add")
+    public int addProduct(@RequestBody ProductDTO productDTO){
+        return productService.insertProduct(productDTO);
+    }
+
+    @PostMapping(value = "/addAndShow")
+    public ProductDTO addProductAndShow(@RequestBody ProductDTO productDTO){
+        int idProduct = productService.insertProduct(productDTO);
+        ProductDTO newProduct = new ProductDTO();
+        newProduct.setId(idProduct);
+        return productService.queryProduct(newProduct);
+    }
+
+    @PutMapping(value = "/update")
+    public int updateProduct(@RequestBody ProductDTO productDTO){
+        return productService.updateProduct(productDTO);
+    }
+
+    @DeleteMapping(value = "/delete")
+    public int deleteProduct(@RequestBody ProductDTO productDTO){
+        return productService.deleteProduct(productDTO);
+    }
+
 }
